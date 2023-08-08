e51f88cc7f2e3311e2f941327364e22c31fae685
Rouxerer <rustynoob011018@gmail.com>
2023-08-09 01:02:29 +0800

    added admin ui

---
+++ admin/tabfinal.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../dist/output.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="flex flex-col justify-center items-center min-h-screen h-screen bg-cover" style="background-image: url('../images/bg.jpg'">
+    <div class="flex justify-center flex-col items-center w-[100%] h-[15%]">
+        <div class="flex justify-center items-center w-[300px] h-[60%] rounded-xl bg-gray-200 mr-[700px] mt-[50px]"><h1 class="font-bold text-4xl">Judge Name</h1>
+        </div>
+    </div>
+<div class="flex flex-row w-[70%] h-[70%] justify-center gap-[30px] items-center">
+    <div class="flex  w-[30%] h-[80%]">
+        <img src="../images/candidates/4.jpg" alt="" class="object-cover">
+    </div>
+    <div class="flex flex-col justify-center items-center  w-[60%] h-[80%] gap-[30px]">
+        <button class="w-[100%] h-[15%] bg-gray-200 rounded-xl text-4xl">Beauty and Brain Result</button>
+        <button class="w-[100%] h-[15%] bg-gray-200 rounded-xl text-4xl">Over all</button>
+    </div>
+</div>
+    
+    
+</body>
+</html>
---
+++ admin/tabgrid.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../dist/output.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="flex flex-col justify-center items-center min-h-screen h-screen bg-cover" style="background-image: url('../images/bg.jpg'">
+    <div class="flex flex-col justify-center items-center w-full h-[80%] gap-[120px]">
+        <div class="flex flex-row justify-center items-center w-[80%] h-[30%] gap-10 mb-5">
+            <div class="flex flex-col justify-center items-center w-[20%]">
+                <img src="../images/logos/zoneIV.png" alt="">
+                <h2 class="text-white font-bold">Gabriela Dorothy Cosico </h2>
+            </div>
+            <div class="flex flex-col justify-center items-center w-[20%]">
+                <img src="../images/logos/zoneIV.png" alt="">
+                <h2 class="text-white font-bold">Gryk Bautista</h2>
+            </div>
+            <div class="flex flex-col justify-center items-center w-[20%]">
+                <img src="../images/logos/zoneIV.png" alt="">
+                <h2 class="text-white font-bold">Emily Jandoni Daban</h2>
+            </div>
+        </div>
+        <div class="flex flex-row justify-center items-center w-[80%] h-[30%] gap-10">
+            <div class="flex flex-col justify-center items-center w-[20%]">
+                <img src="../images/logos/zoneIV.png" alt="">
+                <h2 class="text-white font-bold">Atty. Giovani Garcia</h2>
+            </div>
+            <div class="flex flex-col justify-center items-center w-[20%]">
+                <img src="../images/logos/zoneIV.png" alt="">
+                <h2 class="text-white font-bold text-center">Vice mayor Erlinda ‘Bing pabi’ Araquil</h2>
+            </div>
+            <div class="flex flex-col justify-center items-center w-[20%]">
+                <img src="../images/logos/zoneIV.png" alt="">
+                <h2 class="text-white font-bold">eden jude  Bacas (Thailand Girl)</h2>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    
+</body>
+</html>
